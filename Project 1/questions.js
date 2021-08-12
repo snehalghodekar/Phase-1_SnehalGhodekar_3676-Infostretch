b08f29644d7c3514be85e601553f13521c63c1aa
Snehal_Ghodekar <69964863+snehalghodekar@users.noreply.github.com>
2021-08-13 00:13:10 +0530

    Add files via upload
    All project files added.

---
+++ Project 1/questions.js
@@ -0,0 +1,115 @@
+let questions = [
+    {
+    numb: 1,
+    question: "The correct sequence of HTML tags for starting a webpage is-",
+    answer: "HTML, Head, Title, Body",
+    options: [
+      "Head, Title, HTML, body",
+      "HTML, Body, Title, Head",
+      "HTML, Head, Title, Body",
+      "HTML, Head, Title, Body"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common Style Sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet"
+    ]
+  },
+    {
+    numb: 3,
+    question: "What does PHP stand for?",
+    answer: "Hypertext Preprocessor",
+    options: [
+      "Hypertext Preprocessor",
+      "Hypertext Programming",
+      "Hypertext Preprogramming",
+      "Hometext Preprocessor"
+    ]
+  },
+    {
+    numb: 4,
+    question: "What does SQL stand for?",
+    answer: "Structured Query Language",
+    options: [
+      "Stylish Question Language",
+      "Stylesheet Query Language",
+      "Statement Question Language",
+      "Structured Query Language"
+    ]
+  },
+    {
+    numb: 5,
+    question: "How to create an unordered list in HTML?",
+    answer: "<ul>",
+    options: [
+      "<ul>",
+      "<pl>",
+      "<dl>",
+      "<ol>"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which of the following tag is used to make the underlined text?",
+    answer: "<u>",
+    options: [
+      "<u>",
+      "<i>",
+      "<ol>",
+      "<ul>"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Which of the following tag is used to define options in a drop-down selection list?",
+    answer: "<option>",
+    options: [
+      "<select>",
+      "<list>",
+      "<dropdown>",
+      "<option>"
+    ]
+  },
+  {
+    numb: 8,
+    question: "What does XML stand for?",
+    answer: "eXtensible Markup Language",
+    options: [
+      "eXtensible Markup Language",
+      "eXecutable Multiple Language",
+      "eXTra Multi-Program Language",
+      "eXamine Multiple Language"
+    ]
+  },
+  {
+    numb: 9,
+    question: "HTML tags are enclosed in-",
+    answer: "< and >",
+    options: [
+      "# and #",
+      "{ and }",
+      "! and ?",
+      "< and >"
+    ]
+  },
+{
+    numb: 10,
+    question: "Which of the following attribute is used to provide a unique name to an element?",
+    answer: "id",
+    options: [
+      "class",
+      "id",
+      "type",
+      "None of the above"
+    ]
+  },
+
+
+
+];
